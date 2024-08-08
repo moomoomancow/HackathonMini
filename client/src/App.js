5012079669af9ff8e6811f63b8a7044113dc4f45
Brian <brijump3@gmail.com>
2024-08-08 16:33:51 -0500

    Just got it to fetch the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ChatBotHelper from './chatbot';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ticket, setTicket] = useState([]);
+  const [ticketNumber, setTicketNumber] = useState(2)
+
+  useEffect(() => {
+    TicketFetcher()
+   }, [])
+
+  const TicketFetcher = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/${ticketNumber}`);
+      const data = await response.json();
+      setTicket(data);
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+    if(ticket.length === 0){
+      return <h1>Wait</h1>
+    }
+  }
     return (
         <>
+        {console.log(ticket)}
         <ChatBotHelper/>
+        {ticket.length > 0 ?
+        ticket.map((x) => (
+          <div key = {x.ticketnumber}>
+            <h5>{x.customer}</h5>
+            </div>
+        )):(
+          <p>no ticket</p>
+        )
+     }
         </>
     );
 }
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 // app.js
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV||'development']);
 
 app.use(express.json());
+app.use(cors())
 
-app.get('/', function(req, res) {
+app.get('/:ticketNumber', function(req, res) {
+  const ticket_number = req.params.ticketNumber
   knex('tickets')
     .select('*')
-    .then(data => res.status(200).json({data}))
+    .where('ticketnumber', ticket_number)
+    .then(data => res.status(200).json(data))
     .catch(err =>
       {console.log(err)
         return res.status(404).json({
